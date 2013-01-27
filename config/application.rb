2635cb0fc548f46e62cf7eaf0b51be10b6e97bc3
Christian Neuhaus <pmsfriend@googlemail.com>
2013-01-27 21:08:40 +0100

    working version with upload - baseline

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module CDServer
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile = ['*.js', '*.css']
   end
 end
