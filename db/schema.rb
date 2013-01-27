6657a91728d8caf9ef99023cc80fa0d1d2649483
Christian Neuhaus <pmsfriend@googlemail.com>
2013-01-27 20:26:44 +0100

    working version with upload - baseline

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(:version => 20130102204645) do
   end
 
   add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on"
 
   create_table "tags", :force => true do |t|
     t.string "name"
